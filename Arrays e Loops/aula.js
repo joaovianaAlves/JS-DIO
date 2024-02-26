76099e6c188a6437774dab50b4dc1b4e79395e9c
João Vitor <joaovitorvianaalves@gmail.com>
2024-02-26 17:35:48 -0300

    .

---
+++ Arrays e Loops/aula.js
@@ -0,0 +1,13 @@
+// const alunos = [{ nome: "joao" }, { nome: "andressa" }, { nome: "breno" }];
+// alunos.push({ nome: "alexandre" });
+// alunos.pop();
+// alunos.push({ nome: "alexandre" });
+// alunos.shift();
+// console.log(alunos);
+
+const notas = [];
+
+for (let i = 0; i <= 10; i++) {
+  notas[i] = i;
+}
+console.log(notas);
